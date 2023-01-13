5ea8487ec7172dde8838664f43167f84a9b2abbf
GScalabrini <glscalabrini@gmail.com>
2023-01-13 02:19:13 -0300

    Concluindo a logica de calculo da media

--- src/calculadora/MediaFinal.java
+++ src/calculadora/MediaFinal.java
@@ -1,7 +1,7 @@
 package calculadora;
 
 import java.awt.event.ActionEvent;
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 /* >>>>>> MS = (((P1 + P2) / 2) * 6 + N2 * 4) / 10 + Participação <<<<<< */
 
@@ -24,6 +24,20 @@ public class MediaFinal extends MediaFinalForm{
 
     @Override
     protected void btnCalcularClick(ActionEvent ev) {
-        
+        Calcular();
+    }
+
+    protected void Calcular() {
+        Double p1, p2, n2, part;
+
+        p1 = Double.parseDouble(txtProva1.getText());
+        p2 = Double.parseDouble(txtProva2.getText());
+        n2 = Double.parseDouble(txtN2.getText());
+        part = Double.parseDouble(txtParticipacao.getText());
+
+        Double media = ((((p1 + p2) / 2) * 6 + n2 * 4) / 10 + part);
+
+        //Esta faltando truncar o resultado.
+        txtMs.setText(String.valueOf(media));
     }
 }
